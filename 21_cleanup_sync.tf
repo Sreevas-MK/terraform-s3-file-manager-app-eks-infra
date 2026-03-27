4fbb9d62210ddf3ea306f0f7043fed14e1fe0f6d
Sreevas-MK <sreevasmk@gmail.com>
2026-03-27 01:51:01 +0000

    Third commit - Made changes to destruction anchors

--- 21_cleanup_sync.tf
+++ 21_cleanup_sync.tf
@@ -1,19 +1,45 @@
 resource "null_resource" "destruction_dependencies" {
-  # This list contains everything that can "stall" a delete
+  triggers = {
+    app_name      = var.app_name
+    app_namespace = var.app_namespace
+  }
+
   depends_on = [
     kubectl_manifest.s3_nodejs_app,
-    kubectl_manifest.grafana_datasources,
-    helm_release.monitoring_stack,
     helm_release.argocd,
+    helm_release.monitoring_stack,
     kubernetes_ingress_v1.grafana_ingress,
-    kubernetes_ingress_v1.argocd_shared_ingress,
-    kubernetes_namespace_v1.app_ns,
-    kubernetes_namespace_v1.monitoring,
-    kubernetes_namespace_v1.argocd
+    kubernetes_ingress_v1.argocd_shared_ingress
   ]
 
   provisioner "local-exec" {
-    when    = destroy
-    command = "sleep 180"
+    when = destroy
+
+    command = <<EOT
+      echo "Cleaning ingress finalizers..."
+
+      # App ingress
+      kubectl patch ingress ${self.triggers.app_name}-ingress \
+        -n ${self.triggers.app_namespace} \
+        -p '{"metadata":{"finalizers":[]}}' --type=merge || true
+
+      # ArgoCD ingress
+      kubectl patch ingress argocd-server-ingress \
+        -n argocd \
+        -p '{"metadata":{"finalizers":[]}}' --type=merge || true
+
+      # Grafana ingress
+      kubectl patch ingress grafana-ingress \
+        -n monitoring \
+        -p '{"metadata":{"finalizers":[]}}' --type=merge || true
+
+      echo "Waiting for ALB cleanup..."
+      sleep 120
+    EOT
   }
+}
+
+
+resource "null_resource" "infra_anchor" {
+  depends_on = [null_resource.destruction_dependencies]
 }
